2c8c2be345233f8f32fb3b599a6e9fe464c87672
João <joaosantoos@aol.com>
2021-09-30 11:04:57 -0300

    good enough, huh?

--- main.hs
+++ main.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE FlexibleInstances #-} 
 
 module Main where 
+-- import Data
 import Text.ParserCombinators.Parsec hiding (spaces)
 import System.Environment
 import Control.Monad
@@ -9,16 +10,52 @@ import System.IO
 import Data.IORef
 import Control.Monad.IO.Class
 
+
+
+-- Environment
+
+type Env = IORef [(String, IORef LispVal)]
+
+instance Show (IORef a) where
+    show _ = "<ioref>"
+
+nullEnv :: IO Env
+nullEnv = newIORef []
+
+isBound :: Env -> String -> IO Bool
+isBound envRef var = readIORef envRef >>= return . maybe False
+  (const True) . lookup var
+
+getVar :: Env -> String -> IO LispVal
+getVar envRef var = do
+  env <- readIORef envRef
+  maybe undefined 
+        readIORef
+        (lookup var env)
+
+defineVar :: Env -> String -> LispVal -> IO LispVal
+defineVar envRef var value = do
+  valueRef <- newIORef value
+  env <- readIORef envRef
+  writeIORef envRef ((var, valueRef) : env)
+  return value
+
 -- Data Types
 
-data LispVal = Atom String -- which stores a String naming the atom
-             | List [LispVal] -- which stores a list of other LispVals
-             | DottedList [LispVal] LispVal -- improper list
-             | Number Integer -- containing a Haskell Integer
-             | Float Float -- containing a Haskell float
-             | String String -- containing a Haskell String
-             | Bool Bool -- containing a Haskell boolean value
+data LispVal = Atom String
+             | List [LispVal]
+             | DottedList [LispVal] LispVal
+             | Number Integer
+             | Float Float
+             | String String
+             | Bool Bool
+             | PrimitiveFunc ([LispVal] -> LispVal)
+             | Func { params :: [String], vararg :: (Maybe String),
+                      body :: [LispVal], closure :: Env }
              deriving Show
+
+instance Show ([LispVal] -> LispVal) where
+    show _ = "<primitive>"
 
 -- Parser
 
@@ -125,8 +162,22 @@ numBoolBinop = boolBinop unpackNum
 strBoolBinop = boolBinop unpackStr
 boolBoolBinop = boolBinop unpackBool
 
-apply :: String -> [LispVal] -> LispVal 
-apply func args = maybe (Bool False) ($ args) $ lookup func primitives
+apply :: LispVal -> [LispVal] -> IO LispVal 
+apply (PrimitiveFunc func) args = return $ func args
+apply (Func params vararg body closure) args = do
+  mapM (bindVar closure) (zip params args)
+  eval closure (body !! 0)
+  where bindVar env (p, arg) = defineVar env p arg
+
+primitiveBindings :: IO Env
+primitiveBindings = do
+  env <- nullEnv
+  mapM (makePrimitiveFunc env) primitives
+  return env
+  where makePrimitiveFunc env (var, func) = defineVar env var (PrimitiveFunc func)
+
+makeFunc varargs env params body = return $ Func (map unpackAtom params) varargs body env
+makeNormalFunc = makeFunc Nothing
 
 primitives :: [(String, [LispVal] -> LispVal)]
 primitives = [("+", numericBinop (+)),
@@ -151,15 +202,53 @@ primitives = [("+", numericBinop (+)),
               ("string>=?", strBoolBinop (>=)),
               ("eq?", compareBinop)]
 
-eval :: LispVal -> LispVal
-eval val@(String _) = val
-eval val@(Number _) = val
-eval val@(Bool _) = val
-eval (List [Atom "quote", val]) = val
-eval (List (Atom func : args)) = apply func $ map eval args
+eval :: Env -> LispVal -> IO LispVal
+eval _ val@(Float _) = return val
+eval _ val@(String _) = return val
+eval _ val@(Number _) = return val
+eval _ val@(Bool _) = return val
+eval _ (List [Atom "quote", val]) = return val
+eval env (Atom id) = getVar env id
+eval env (List [Atom "define", Atom var, form]) = 
+  eval env form >>= defineVar env var
+eval env (List (Atom "define" : List (Atom var : params) : body)) =
+  makeNormalFunc env params body >>= defineVar env var
+eval env (List [Atom "if", pred, conseq, alt]) = do
+  result <- eval env pred
+  case result of
+    Bool False -> eval env alt
+    otherwise -> eval env conseq
+eval env (List (Atom "cond" : pairs)) = evalCond pairs
+    where evalCond (List [Atom "else", value] : []) = eval env value
+          evalCond (List [condition, value] : rest) = do
+            conditionResult <- eval env condition
+            case conditionResult of
+                Bool False -> evalCond rest
+                _ -> eval env value
+          evalCond [] = pure $ Atom ""
+eval env (List (Atom func : args)) = do
+ x <- mapM (eval env) args
+ f <- (getVar env func)
+ apply f x
+
+-- REPL
+
+readPrompt :: String -> IO String
+readPrompt prompt = putStr prompt >> hFlush stdout >> getLine
+
+evalString :: Env -> String -> IO String
+evalString env expr = liftM show $ eval env $ readExpr expr
+
+evalAndPrint :: Env -> String -> IO ()
+evalAndPrint env expr = evalString env expr >>= putStrLn
+
+until_ pred prompt action = do
+  result <- prompt
+  if pred result
+    then return ()
+    else action result >> until_ pred prompt action
 
 main :: IO ()
 main = do
-  (expr:_) <- getArgs
-  putStrLn $ show $ eval $ readExpr expr
-
+  env <- primitiveBindings
+  until_ (== "quit") (readPrompt "Lisp>>> ") (evalAndPrint env)
